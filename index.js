69c74748bb98fbdb6488e733b54f62a33ef46e0a
Amdadul-haq <amdadulhaq3746@gmail.com>
2025-01-16 01:14:33 +0600

    Update puppeter launch

--- index.js
+++ index.js
@@ -308,7 +308,10 @@ app.post("/add-prescription", async (req, res) => {
     });
 
     // Launch Puppeteer and generate PDF
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+
     const page = await browser.newPage();
 
     await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
